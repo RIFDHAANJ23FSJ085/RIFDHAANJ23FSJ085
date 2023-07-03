117506cc34ab33cba4d4af1f846bffefbde498a2
Rifdhan <138508786+RIFDHAANJ23FSJ085@users.noreply.github.com>
2023-07-03 21:33:50 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,13 @@
+package openlabbook1;
+
+public class Addition {
+public static void main(String[] args) {
+	System.out.println("Enter two number..");
+	
+	int first=100;
+	int second=200;
+	System.out.println(first+" "+second);
+	int sum=first+second;
+	System.out.println("The Sum Is : "+sum);
+}
+}
